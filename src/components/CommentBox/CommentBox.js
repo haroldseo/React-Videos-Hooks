0dd8ddd5247bc7005eeaaa86ff62e3c2b20cd93f
Harold Seo <harold.seo.612@gmail.com>
2021-10-08 14:07:35 -0700

    Add Form Validation

--- src/components/CommentBox/CommentBox.js
+++ src/components/CommentBox/CommentBox.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 
-const CommentBox = ({ comments, setComments, addComment }) => {
+const CommentBox = ({ addComment }) => {
   const [commentValue, setCommentValue] = useState({ author: "", comment: "" });
   const author = commentValue.author;
   const comment = commentValue.comment;
+
+  const [commentErr, setCommentErr] = useState({});
 
   const handleUserInput = (event) => {
     setCommentValue({ ...commentValue, [event.target.name]: event.target.value });
@@ -12,21 +14,48 @@ const CommentBox = ({ comments, setComments, addComment }) => {
   const handleCommentSubmit = (event) => {
     event.preventDefault();
 
-    addComment(commentValue);
-    setCommentValue({ ...commentValue, author: "", comment: "" });
+    const isValid = formValidation();
+
+    if (isValid) {
+      addComment(commentValue);
+      setCommentValue({ ...commentValue, author: "", comment: "" });
+    }
+  };
+
+  const formValidation = () => {
+    const commentErr = {};
+    let isValid = true;
+
+    if (!author) {
+      commentErr.author = "Name is required!";
+      isValid = false;
+    }
+
+    if (!comment) {
+      commentErr.comment = "Comment is required!";
+      isValid = false;
+    } else if (comment.length > 300) {
+      commentErr.comment = "You have exceeded the maximum character limit: 300";
+      isValid = false;
+    }
+
+    setCommentErr(commentErr);
+    return isValid;
   };
 
   return (
     <form onSubmit={handleCommentSubmit} className='ui form'>
       <div className='field'>
-        <div className='ui action input'>
-          <input type='text' placeholder='Name' value={author} name='author' onChange={handleUserInput} />
-          <input type='text' placeholder='Add Comment' value={comment} name='comment' onChange={handleUserInput} />
-          <button className='ui button' type='submit'>
-            Post
-          </button>
-        </div>
+        <input type='text' placeholder='Name' value={author} name='author' onChange={handleUserInput} />
+        {commentErr.author}
       </div>
+      <div className='field'>
+        <textarea rows='3' type='text' placeholder='Add Comment' value={comment} name='comment' onChange={handleUserInput} />
+        {commentErr.comment}
+      </div>
+      <button className='ui button' type='submit'>
+        Post
+      </button>
     </form>
   );
 };
