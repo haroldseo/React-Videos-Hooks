e2ec8e7f7269b1c0ff73bfd9b35c988cb935c181
Harold Seo <harold.seo.612@gmail.com>
2022-01-06 00:25:09 -0800

    Refactor SearchBar Component to TypeScript

--- src/hooks/useVideos.tsx
+++ src/hooks/useVideos.tsx
@@ -2,15 +2,13 @@ import { useState, useEffect } from "react";
 import youtube from "../apis/youtube";
 
 const useVideos = (defaultSearchTerm: string) => {
-  //String
-  const [videos, setVideos] = useState<object[]>([]); //Array of objects
+  const [videos, setVideos] = useState<object[]>([]);
 
   useEffect(() => {
     search(defaultSearchTerm);
   }, [defaultSearchTerm]);
 
   const search = async (term: string) => {
-    //String
     const response = await youtube.get("/search", {
       params: {
         q: term,
